33118ab9b1a14573b5eb4345439d6703915ed2bb
2ushar03 <tusharbpatil5784@gmail.com>
2025-03-28 19:40:37 +0530

    Updated all files

---
+++ src/data.js
@@ -0,0 +1,127 @@
+import SpicyPaneerWrapImage from './resources/Spicypannerwrap.png';
+import IceCreamImage from './resources/Icecream.png';
+import PaavBhajiImage from './resources/paavbhaji.png';
+import CakeAndDesertImage from './resources/cakeanddesert.png';
+import DosaImage from './resources/dosa.png';
+import IdliImage from './resources/idli.png';
+import JuiceImage from './resources/juice.png';
+import MomosImage from './resources/momos.png';
+import AlooTikiBurgerImage from './resources/AlooTikiBurger.avif';
+import MargrettaImage from './resources/Margretta.avif';
+import McSpicyChickenImage from './resources/McSpicyChicken.avif';
+import PeriPeriPizzaImage from './resources/Periperi.avif';
+import VegcornImage from './resources/Vegcorn.avif';
+import CheesePizzaImage from './resources/cheese.avif';
+import VegRollImage from './resources/vegroll.png';
+
+const data = {
+  productItems: [
+    {
+      id: "1",
+      title: "Big Spicy Paneer Wrap Combo",
+      info: "Enjoy Panner Wrap + Fries + Drink",
+      price: 360.5,
+      image: SpicyPaneerWrapImage
+    },
+    {
+      id: "13",
+      title: "Ice Cream",
+      info: "Yummy!...",
+      price: 60,
+      image: IceCreamImage
+    },
+    {
+      id: "14",
+      title: "Paav Bhaji",
+      info: "Tasty!...",
+      price: 89,
+      image: PaavBhajiImage
+    },
+    {
+      id: "2",
+      title: "Cake And Desert",
+      info: "Enjoy Cake For Celebrations Buy1 Get 1 free",
+      price: 380,
+      image: CakeAndDesertImage
+    },
+    {
+      id: "3",
+      title: "Dosa",
+      info: "Yummy!...",
+      price: 80,
+      image: DosaImage
+    },
+    {
+      id: "4",
+      title: "Idli",
+      info: "Enjoy Idli",
+      price: 30,
+      image: IdliImage
+    },
+    {
+      id: "5",
+      title: "Juice",
+      info: "Enjoy Juice To Be Fit",
+      price: 45,
+      image: JuiceImage
+    },
+    {
+      id: "6",
+      title: "Momos",
+      info: "Enjoy Momos",
+      price: 70,
+      image: MomosImage
+    },
+    {
+      id: "7",
+      title: "2 McAloo Tikki Burger + 2 Fries",
+      info: "Enjoy Tikki Burger with fries",
+      price: 327.95,
+      image: AlooTikiBurgerImage
+    },
+    {
+      id: "8",
+      title: "Margreta",
+      info: "Enjoy Margreta Pizza",
+      price: 349,
+      image: MargrettaImage
+    },
+    {
+      id: "9",
+      title: "Spicy Chicken Burger",
+      info: "Enjoy Chicken Burger + fries +Drink",
+      price: 388.95,
+      image: McSpicyChickenImage
+    },
+    {
+      id: "10",
+      title: "Peri Peri Pizza - Freshly Hand Tossed 7",
+      info: "Enjoy Peri Peri Pizza",
+      price: 359,
+      image: PeriPeriPizzaImage
+    },
+    {
+      id: "11",
+      title: "New York Style Cheese",
+      info: "Enjoy the Classic New York Pizza! Topped with 100% whole milk Mozzarella",
+      price: 199,
+      image: VegcornImage
+    },
+    {
+      id: "12",
+      title: "Veg Corn Pizza",
+      info: "Enjoy Veg Corn Pizza",
+      price: 200,
+      image: CheesePizzaImage
+    },
+    {
+      id: "15",
+      title: "Veg Roll",
+      info: "Yummy!...",
+      price: 80,
+      image: VegRollImage
+    },
+  ]
+};
+
+export default data;
